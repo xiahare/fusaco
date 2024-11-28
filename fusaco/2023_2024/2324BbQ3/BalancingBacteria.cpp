8b55582106090391161a7763fe44d9ce653cd2c5
xiahare <xiahare@163.com>
2024-11-27 23:54:12 -0800

    2324BbQ3 complete answer, but I don's agree with the answer

--- fusaco/2023_2024/2324BbQ3/BalancingBacteria.cpp
+++ fusaco/2023_2024/2324BbQ3/BalancingBacteria.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -7,29 +7,41 @@ using ll=long long;
 
 int main() {
     
+//    ifstream cin("/Users/lei/Downloads/prob3_bronze_jan24/12.in");
+//    ofstream cout("/Users/lei/Downloads/prob3_bronze_jan24/12.x1");
+    
     int n;
     cin >> n;
-    ll a[n];
-    for(int i=0; i<n;i++){
-        cin >> a[i];
-    }
     
     ll steps = 0;
     ll total_d=0,subtotal_d=0;
 
+    ll a;
+    /**
+    That's very intresting.
+    For example,
+     2
+     3 4
+     The result will get "5".
+     It means 3 operations to {0,-2} , and then need 2 operations to {0.0}
+     
+    I think we can just choose L=4, which means just need only 1 time to do so.
+    I wonder why it could not?
+    It doesn't say it cannot spray out of the region.
+     
+     */
+//    cin >> a;
+//    if(a!=0){
+//        steps++; // only once
+//
+//        total_d = 0-a;
+//        subtotal_d = total_d<0?-1:1;
+//    }
     
-    if(a[0]!=0){
-
-        steps++; // only once
-
-        total_d = 0-a[0];
-        subtotal_d = total_d<0?-1:1;
-    }
-    
-    for(int i=1; i<n;i++){
-        
+    for(int i=0; i<n;i++){
+        cin >> a;
         // calculate
-        ll delt = 0 - total_d - subtotal_d - a[i] ;
+        ll delt = 0 - total_d - subtotal_d - a ;
         steps = steps + abs(delt);
         
         // for next
@@ -53,4 +65,9 @@ int main() {
 1 3 -2 -7 5
 ------
 26
+ 
+ 2
+ 5 10
+ -----
+ 
  */
