99ff21cd52831c15a0ae1c048a4f272954bd2d83
xiahare <xiahare@163.com>
2024-11-27 21:05:31 -0800

    2324BbQ3 still 3 errors

--- fusaco/2023_2024/2324BbQ3/BalancingBacteria.cpp
+++ fusaco/2023_2024/2324BbQ3/BalancingBacteria.cpp
@@ -6,34 +6,36 @@ using namespace std;
 using ll=long long;
 
 int main() {
+    
     int n;
     cin >> n;
-    vector<ll> a(n);
+    ll a[n];
     for(int i=0; i<n;i++){
         cin >> a[i];
     }
+    
     ll steps = 0;
     ll total_d=0,subtotal_d=0;
+
+    
     if(a[0]!=0){
-        steps++;
-        ll delt = 0-a[0];
-        total_d=delt;
-        subtotal_d=delt;
-        for(int i=1; i<n;i++){
-            int sign = delt<0?-1:1;
-            a[i] = a[i] + delt + i*sign;
-        }
+
+        steps++; // only once
+
+        total_d = 0-a[0];
+        subtotal_d = total_d<0?-1:1;
     }
     
     for(int i=1; i<n;i++){
-        if(a[i]==0) continue;
         
-        ll delt = 0-a[i];
+        // calculate
+        ll delt = 0 - total_d - subtotal_d - a[i] ;
         steps = steps + abs(delt);
-        a[i]=0;
-        for(int j=i+1; j<n; j++){
-            a[j] = a[j] + delt*(j-i+1);
-        }
+        
+        // for next
+        subtotal_d = subtotal_d + delt;
+        total_d = total_d + subtotal_d;
+
     }
     
     cout << steps << endl;
