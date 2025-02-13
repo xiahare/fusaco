6ac8e712180b125a5cb3d3f1cce8562b76d421d0
Lei Xia <leixia@fortinet.com>
2025-02-12 21:31:23 -0800

    2425 r2 sq2

--- fusaco/2024_2025/2425S2Q2/main.cpp
+++ fusaco/2024_2025/2425S2Q2/main.cpp
@@ -1,41 +1,71 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <algorithm>
 #include <climits>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int T;
     cin >> T;
 
     while (T--) {
-        int N;
-        long long M;
+        int N, M;
         cin >> N >> M;
 
-        vector<long long> a(N);
-        vector<long long> freq(M, 0);
+        map<ll, int> mcnt; // (ai%M,count)
+
+        // Calculate frequencies of a[i] % M
+        int a = 0;
         for (int i = 0; i < N; ++i) {
-            cin >> a[i];
-            freq[a[i] % M]++;
+            cin >> a;
+            mcnt[a % M]++;
         }
 
-        long long minOperations = LLONG_MAX;
+        int cptN = mcnt.size();
+        int extN = 2*cptN;
+        vector<ll> aMoExt(extN+1,0LL);
+        vector<ll> psum(extN+1,0LL);
+        vector<ll> pcnt(extN+1,0LL);
 
-        // Try every possible x % M
-        for (int xModM = 0; xModM < M; ++xModM) {
-            long long operations = 0;
+        int k=1;
+        for(auto &[aMo,cnt]: mcnt ){
+            aMoExt[k] = aMo;
+            psum[k] = psum[k-1] + aMo*cnt;
+            pcnt[k] = pcnt[k-1] + cnt;
+            k++;
+        }
+        for(auto &[aMo,cnt]: mcnt ){
+            aMoExt[k] = aMo + M;
+            psum[k] = psum[k-1] + (aMo + M)*cnt;
+            pcnt[k] = pcnt[k-1] + cnt;
+            k++;
+        }
+        
+        ll minOpts = LLONG_MAX;
+        int left = M/2, right = (M-1)/2;
+        for(int i=1; i<=extN; i++){
+            int x = aMoExt[i];
+            if(x-left >=0 && x+right<2*M){ // odd: ...|... , even ....|...
+                int idx_left = lower_bound(aMoExt.begin()+1, aMoExt.end(), x-left) - aMoExt.begin();
+                ll sum_left = psum[i-1] - psum[idx_left-1];
+                ll cnt_left = pcnt[i-1] - pcnt[idx_left-1];
 
-            for (int r = 0; r < M; ++r) {
-                // Calculate cost to adjust residue r to xModM
-                long long cost = min((r - xModM + M) % M, (xModM - r + M) % M);
-                operations += freq[r] * cost;
+                int idx_right = upper_bound(aMoExt.begin()+1, aMoExt.end(), x+right) - aMoExt.begin() -1;
+                ll sum_right = psum[idx_right] - psum[i] ;
+                ll cnt_right = pcnt[idx_right]- pcnt[i];
+
+                minOpts = min( minOpts, x*cnt_left - sum_left + sum_right - x*cnt_right);
             }
-
-            minOperations = min(minOperations, operations);
         }
 
-        cout << minOperations << endl;
+        cout << minOpts << endl;
     }
 
     return 0;
@@ -46,10 +76,16 @@ int main() {
 15 12 18 3 8
 3 69
 1 988244353 998244853
-
-
 ----
 10
 21
  
+2
+5 6
+15 12 18 3 8
+2 6
+15 12
+----
+6
+3
  */
